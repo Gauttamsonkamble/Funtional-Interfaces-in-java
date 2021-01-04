4bc3b3cf40de96ba75ce1b449cffbeee0dc05ffd
Gauttam Sonkamble <46339795+Gauttamsonkamble@users.noreply.github.com>
2021-01-04 20:38:20 +0530

    Add files via upload

--- Java_8_Stream_API/src/com/Gauttam/Stream_API_1.java
+++ Java_8_Stream_API/src/com/Gauttam/Stream_API_1.java
@@ -6,9 +6,11 @@ import java.util.List;
 public class Stream_API_1 {
 
 	public static void main(String[] args) {
-		List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6,7);
+		List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6);
 		
-		System.out.println(values.stream().map(i->i*2).reduce(0,(c,e)->c+e));
+//		System.out.println(values.stream().map(i->i*2).reduce(0,(c,e)->c+e));
+		
+		System.out.println(values.stream().map(i->i*2).reduce(0,Integer::sum));
 
 	}
 
